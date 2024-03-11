edfa7ad2a198cf42fa9cdcd80128f7c4f1a083f7
Chris He <hwachris@amazon.com>
2024-03-11 13:01:24 +0800

    Convert UI from gr.ChatInterface to gr.Blocks.

--- src/ui/gradio/chatbot.py
+++ src/ui/gradio/chatbot.py
@@ -13,21 +13,45 @@ class GradioUI:
             history = history or []
             response = boto3Bedrock.get_conversation().predict(input=message)
             history.append((message, response))
-            return response
+            return '', history
 
-        return gr.ChatInterface(
-            predict,
-            chatbot=gr.Chatbot(height=300),
-            textbox=gr.Textbox(placeholder="Ask me questions", container=False, scale=7),
-            title="Amazon Bedrock",
-            description="GUI for Amazon Bedrock",
-            # theme="soft",
-            # examples=["Hello", "Am I cool?", "Are tomatoes vegetables?"],
-            # cache_examples=True,
-            retry_btn=None,
-            undo_btn="Delete Previous",
-            clear_btn="Clear",
-        )
+        with gr.Blocks() as chatUI:
+            chatbot = gr.Chatbot(
+                [],
+                elem_id="chatbot",
+                bubble_full_width=False,
+            )
+
+            with gr.Row():
+                txt = gr.Textbox(
+                    scale=4,
+                    show_label=False,
+                    placeholder="Enter text and press enter, or upload an image",
+                    container=False,
+                )
+                btn = gr.UploadButton("📁", file_types=["image", "video", "audio"])
+
+            # txt_msg = txt.submit(predict, [chatbot, txt], [chatbot, txt], queue=False).then(
+            #     bot, chatbot, chatbot, api_name="bot_response"
+            # )
+
+            txt_msg = txt.submit(predict, [txt, chatbot], [txt, chatbot], queue=False)
+
+        return chatUI
+
+        # return gr.ChatInterface(
+        #     predict,
+        #     chatbot=gr.Chatbot(height=300),
+        #     textbox=gr.Textbox(placeholder="Ask me questions", container=False, scale=7),
+        #     title="Amazon Bedrock",
+        #     description="GUI for Amazon Bedrock",
+        #     # theme="soft",
+        #     # examples=["Hello", "Am I cool?", "Are tomatoes vegetables?"],
+        #     # cache_examples=True,
+        #     retry_btn=None,
+        #     undo_btn="Delete Previous",
+        #     clear_btn="Clear",
+        # )
 
     def launch(self):
         self.ui.launch()
